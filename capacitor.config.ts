2f0eb0a1e10357e13d8884e993c49ac17d0baee2
TimsReef <tgizinski@outlook.com>
2026-03-15 23:22:48 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tiptacular.app',
   appName: 'Tiptacular',
-  webDir: 'www'
+  webDir: 'www',
+  backgroundColor: '#ffffff', // Sets the default background color
+  android: {
+    backgroundColor: 'transparent'
+  },
+  ios: {
+    backgroundColor: 'transparent'
+  }
 };
 
 export default config;
