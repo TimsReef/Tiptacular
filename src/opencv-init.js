f346176df81322e1a484df86bee219e20b656992
TimsReef <tgizinski@outlook.com>
2026-03-20 09:55:30 -0400

    Debugging Receipt Scanning

--- src/opencv-init.js
+++ src/opencv-init.js
@@ -1,8 +1,10 @@
 import cv from "@techstark/opencv-js"
 
+let cvReady=false;
 window.cv = cv
 
 // Wait until OpenCV runtime is ready
 cv.onRuntimeInitialized = () => {
   logScan("OpenCV ready");
+  cvReady=true;
 }
