c48652ea23917f6c768bcb6081316f8d60a56179
TimsReef <tgizinski@outlook.com>
2026-03-20 23:50:41 -0400

    Trying to fix version

--- www/about.js
+++ www/about.js
@@ -10,7 +10,7 @@ let debugMode=false;
 
 async function openAbout(){
  if(window.Capacitor) {
-   const info = await Capacitor.App.getInfo();
+   const info = await window.Capacitor.App.getInfo();
    APP_VERSION = info.version;
  }
  const modal=document.createElement('div');
