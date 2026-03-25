00849cfaf144ca0a7b0ea0e810e8ec4af465f321
TimsReef <tgizinski@outlook.com>
2026-03-24 22:03:32 -0400

    fixing build

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require("path");
 
 module.exports = {
   entry: {
-      opencv: "./src/opencv-init.js",
+      opencv: "./src/opencv-init.js", // Creates 'opencv.bundle.js' (or as specified below)
       capacitor: "./src/capacitor-init.js",
   },
 
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js", // [name] is replaced by the entry key (main, analytics)
     path: path.resolve(__dirname, "./www/js")
   },
 
