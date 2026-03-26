3fb33456958d73c8e7852704b2945ad0318da89d
TimsReef <tgizinski@outlook.com>
2026-03-25 23:41:33 -0400

    Fixing opencv initialization

--- src/opencv-init.js
+++ src/opencv-init.js
@@ -1,10 +1,9 @@
 import cv from "@techstark/opencv-js"
 
-let cvReady=false;
+export let cvReady=false;
 window.cv = cv
 
 // Wait until OpenCV runtime is ready
 cv.onRuntimeInitialized = () => {
-  logScan("OpenCV ready");
   cvReady=true;
 }
