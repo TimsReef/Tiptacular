d6a70b564a01622b90983c8793038bfac7d95967
TimsReef <tgizinski@outlook.com>
2026-03-28 13:10:54 -0400

    Fixing logging of metrics

--- www/utils.js
+++ www/utils.js
@@ -7,8 +7,10 @@ function dataToString(obj){
   let outputString = "";
 
   if (typeof obj === 'object' && obj !== null) { // Check for object type
-    if (Object.keys(obj).length !== 0) // Check for empty object
+    if (Object.keys(obj).length > 0) // Check for empty object
       outputString = JSON.stringify(obj, null, 2);
+    else
+      outputString = undefined;
   } else {
     outputString = String(obj); // Convert other types (like number) to a string
   }
