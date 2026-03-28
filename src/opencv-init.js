e990de47c3cec907efa271258dcfd14fe4350dd0
TimsReef <tgizinski@outlook.com>
2026-03-28 11:41:41 -0400

    Attach opencv to window

--- src/opencv-init.js
+++ src/opencv-init.js
@@ -1,17 +1,25 @@
 import cv from "@techstark/opencv-js"
 
 // Initialize OpenCV and return the cv object
-export async function initializeOpenCV() {
-  let cvReady;
-  if (cv instanceof Promise) {
-    cvReady = await cv;
-  } else {
-    // Fallback for older versions or specific configurations
-    await new Promise((resolve) => {
-      cv.onRuntimeInitialized = () => resolve();
-    });
-    cvReady = cv;
-    window.cv = cv;
-  }
-  return cvReady;
+//export async function initializeOpenCV() {
+//  let cvReady;
+//  if (cv instanceof Promise) {
+//    cvReady = await cv;
+//  } else {
+//    // Fallback for older versions or specific configurations
+//    await new Promise((resolve) => {
+//      cv.onRuntimeInitialized = () => resolve();
+//    });
+//    cvReady = cv;
+//    window.cv = cv;
+//  }
+//  return cvReady;
+//}
+
+
+window.cv = cv
+
+// Wait until OpenCV runtime is ready
+cv.onRuntimeInitialized = () => {
+  Console.log("OpenCV Ready");
 }
